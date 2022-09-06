2f88fef3c3cc0d86c2373abd620b19190360a145
xulihang <xulihanghai@163.com>
2022-09-06 02:28:47 -0700

    get folder and port from arguments

--- src/ios/GCDServer.m
+++ src/ios/GCDServer.m
@@ -15,12 +15,22 @@
     CDVPluginResult* pluginResult = nil;
     if (_webServer == nil) {
         _webServer = [[GCDWebServer alloc] init];
+        
+        NSDictionary* dict = [command.arguments objectAtIndex:0];
+        NSString * folder = @"www";
+        if (dict[@"folder"] != nil) {
+            folder = dict[@"folder"];
+        }
+        int port = 8080;
+        if (dict[@"port"] != nil) {
+            port = [dict[@"port"] intValue];
+        }
         NSBundle* mainBundle;
         // Get the main bundle for the app.
         mainBundle = [NSBundle mainBundle];
-        NSString *path = [mainBundle pathForResource:@"www" ofType:nil];
+        NSString *path = [mainBundle pathForResource:folder ofType:nil];
         [_webServer addGETHandlerForBasePath:@"/" directoryPath:path indexFilename:nil cacheAge:3600 allowRangeRequests:YES];
-        [_webServer startWithPort:8080 bonjourName:nil];
+        [_webServer startWithPort:port bonjourName:nil];
         NSLog(@"path: %@",path);
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"okay"];
     }else{
